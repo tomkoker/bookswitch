5b2b41daf3d676a1419d9f001e9017e7cf72bb62
Robaire <galliath.robaire@gmail.com>
2017-05-27 23:15:51 -0700

    Map out process for cleaning up some HTML using Vue. Beginning Account page.

--- client/components/Account.vue
+++ client/components/Account.vue
@@ -1,8 +1,38 @@
 <template>
+  <div id='account'>
+  	<div class="row">
 
-This is template.
+      <!-- Menu Bar -->
+	  <div class="col-xs-2">
+	    <ul>
+	      <li> Stuff </li>
+	      <li> More Stuff </li>
+	      <li> Other Stuff </li>
+	      <li> Not Stuff </li>
+	    </ul>
+	  </div>
 
-</template
+	  <!-- Content -->
+	  <div class="col-xs-10">
+	  	<div class="row">
+
+	  	  <!-- Greeting Message -->
+	  	  <div class="col-xs-12">
+	  	    <p v:text="USER NAME"> Welcome + USER NAME HERE </p> <!-- Use a v:text to adjust the content the user sees. -->
+	  	  </div>
+
+	  	  <!-- Selection dependent information -->
+	  	  <!-- It might be best to use nested routers here -->
+	  	  <div class="col-xs-12">
+	  	  </div>
+
+
+	  	</div>
+	  </div>
+
+	</div>
+  </div>
+</template>
 
 <script>
 </script>
